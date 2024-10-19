b354ee5e7c4172f09a2df6c8300863e4ba595b2e
Multiplicit.co.uk <127529943+multiplicit-com@users.noreply.github.com>
2024-10-19 15:24:58 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 // test.js on GitHub
+console.log('Script loaded on page:', window.location.pathname);
+
 const uniqueID = 'HARDCODED_UNIQUE_ID';
 
-// Display a basic alert to test if the script is working on any page
-alert('Script is working on all pages! Your unique ID is: ' + uniqueID);
+if (window.location.pathname.includes('/thank-you')) {
+  alert('Script is working on the Thank You page! Your unique ID is: ' + uniqueID);
+} else {
+  console.log('This is not the Thank You page');
+}

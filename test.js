a9672e1cf25962ffe70e4f81e74b753ab1702928
Multiplicit.co.uk <127529943+multiplicit-com@users.noreply.github.com>
2024-10-20 12:21:00 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-// test.js on GitHub
-console.log('Script loaded on page:', window.location.pathname);
-
-const uniqueID = 'HARDCODED_UNIQUE_ID';
-
-if (window.location.pathname.includes('/thank-you')) {
-  alert('Script is working on the Thank You page! Your unique ID is: ' + uniqueID);
-} else {
-  console.log('This is not the Thank You page');
-}
-
 (function(i){
   if(i&&i.a.length>0){
     var c=document;
@@ -70,3 +59,4 @@ if (window.location.pathname.includes('/thank-you')) {
     a();
   }
 })(function(){var e="dDropI";var r=window[e[4]+e[2]+e[3]+e[0]+e[5]+e[1]];return typeof r!==undefined?r:false}());
+
